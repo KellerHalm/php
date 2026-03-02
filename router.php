7dbf83ab650e318030e098963ea013ea2c062fc3
DUM <uveis2005@yandex.ru>
2026-03-02 13:34:47 +0300

    Create router.php

---
+++ router.php
@@ -0,0 +1,12 @@
+<?php
+
+    // if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+    //     return false;
+    // } else {
+    //     echo "<p> Welcome from PHP </p>";
+    // }
+
+    $today = getdate();
+    print_r($today);
+
+?>
