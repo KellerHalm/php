9c471e9e15b0bcaadb5e2c331eac704b77b36c80
DUM <uveis2005@yandex.ru>
2026-03-23 14:00:45 +0300

    Update oop.php

--- oop.php
+++ oop.php
@@ -228,4 +228,116 @@
     // {} 
 
 
+    // class Person 
+    // {
+    //     private $id = '123';
+    //     public $name = 'Uveis';
+    //     protected $email = 'uveis@mail.ru';
+
+    //     private function getId() 
+    //     {
+    //         echo "This private method for get ID <br />";
+    //     }
+
+    //     public function getName() 
+    //     {
+    //         echo "This public method for get name <br />";
+    //     }
+
+    //     protected function getEmail() 
+    //     {
+    //         echo "This protected method for get email <br />";
+    //     }
+
+    //     function test()
+    //     {
+    //         $this -> getId();
+    //         $this -> getName();
+    //         $this -> getEmail();
+
+    //         echo "$this->id <br />";
+    //         echo "$this->name <br />";
+    //         echo "$this->email <br />";
+    //     }
+    // }
+
+    // $person = new Person();
+    
+    // echo $person -> test();
+    // echo "$person->name";
+
+
+    // class Employee extends Person
+    // {
+    //     function test()
+    //     {
+    //         $this -> getName();
+    //         $this -> getEmail();
+    //     }
+    // }
+
+
+    // class Person 
+    // {
+    //     public $name, $age;
+
+    //     static $id = 0;
+
+    //     function __construct($name, $age)
+    //     {
+    //         self::$id++;
+    //         $this -> name = $name;
+    //         $this -> age = $age;
+    //     }
+
+    //     function getID()
+    //     {
+    //         return self::$id;
+    //     }
+    // }
+
+    // $person = new Person("Uveis", 20);
+    // echo $person -> name . " " . $person -> age . " " . $person -> getID();
+
+    // $person2 = new Person("Uveys", 21);
+    // echo $person2 -> name . " " . $person2 -> age . " " . $person2 -> getID();
+
+
+    // trait Printer
+    // {
+    //     public function printSimpleText1($newParams)
+    //     {
+    //         echo "Method 1 $newParams <br>";
+    //     }
+
+    //     public function printSimpleText2($newParams)
+    //     {
+    //         echo "Method 2 $newParams <br>";
+    //     }
+    // }
+
+    // class Message
+    // {
+    //     use Printer;
+    // }
+
+    // $message = new Message();
+    // $message -> printSimpleText1("1");
+    // $message -> printSimpleText2("2");
+
+
+    // class Person 
+    // {
+    //     public $id = 1;
+    // }
+
+    // $person = new Person();
+    // $person->id = 123;
+
+    // $per = clone $person;
+    // $per->id = 0;
+
+    // echo $per->id . "<br>" . $person->id;
+
+
 ?>
